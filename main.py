7087de8b49a0c14383a156f5e2c278f5c781c122
void-hoge <coilgunrailgun@gmail.com>
2021-11-17 08:38:37 +0900

    add main function

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def add_classicon(base, shipclass, color, fontsize):
 	font = ImageFont.truetype(fontpath, fontsize)
 	draw = ImageDraw.Draw(base)
 	size = draw.textsize(shipclass, font=font)
-	pos = tuple([(a-b)/2 for a, b in zip(img_size, size)])
+	pos = tuple([(a-b)//2 for a, b in zip(img_size, size)])
 	draw.text(pos, shipclass, font=font, fill=color)
 	base = base.rotate(90)
 	return base
@@ -31,34 +31,33 @@ def add_shipname(base, name, color, fontsize):
 	while draw.textsize(name, font=font)[0]>img_size[0]:
 		name = name[:-1]
 	size = draw.textsize(name, font=font)
-	pos = tuple([(a-b)/2 for a, b in zip(img_size, size)])
+	pos = tuple([(a-b)//2 for a, b in zip(img_size, size)])
 	pos = (pos[0],pos[1]+125)
 	draw.text(pos, name, font=font, fill=color)
 	return base
 
-argv = sys.argv
-if (argv[1] == "CV") or argv[1] == "cv":
-	shipclass = CV
-elif (argv[1] == "BB") or (argv[1] == "bb"):
-	shipclass = BB
-elif (argv[1] == "CA") or (argv[1] == "CL") or (argv[1] == "ca") or (argv[1] == "cl"):
-	shipclass = CA
-elif (argv[1] == "DD") or (argv[1] == "dd"):
-	shipclass = DD
-else:
-	raise Exception("invalid class name. (CV|BB|CA|CL|DD|cv|bb|ca|cl|dd)")
+def main():
+	argv = sys.argv
+	if (argv[1] == "CV") or argv[1] == "cv":
+		shipclass = CV
+	elif (argv[1] == "BB") or (argv[1] == "bb"):
+		shipclass = BB
+	elif (argv[1] == "CA") or (argv[1] == "CL") or (argv[1] == "ca") or (argv[1] == "cl"):
+		shipclass = CA
+	elif (argv[1] == "DD") or (argv[1] == "dd"):
+		shipclass = DD
+	else:
+		raise Exception("Invalid class name. (CV|BB|CA|CL|DD|cv|bb|ca|cl|dd)")
+	if (argv[2] == "enemy"):
+		color = enemy
+	elif (argv[2] == "friend"):
+		color = friend
+	name = argv[3]
+	base = Image.new("RGBA", img_size, color=base_color)
+	base = add_classicon(base, shipclass, color, icon_size)
+	base = add_shipname(base, name, color, 75)
+	filename = argv[4]
+	base.save(filename)
 
-if (argv[2] == "enemy"):
-	color = enemy
-elif (argv[2] == "friend"):
-	color = friend
-
-name = argv[3]
-
-base = Image.new("RGBA", img_size, color=base_color)
-base = add_classicon(base, shipclass, color, icon_size)
-base = add_shipname(base, name, color, 75)
-
-filename = argv[4]
-
-base.save(filename)
+if __name__ == '__main__':
+	main()
