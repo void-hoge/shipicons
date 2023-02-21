ddfbfb3d754cd2ed998bb5675f0341cc3648e169
void-hoge <coilgunrailgun@gmail.com>
2023-02-22 00:32:38 +0900

    compatible with pillow 10

--- main.py
+++ main.py
@@ -14,50 +14,54 @@ BB = "1"
 CA = "2"
 DD = "3"
 
+def get_textsize(font, text):
+    left, top, right, bottom = font.getbbox(text)
+    return right - left, bottom - top
+
 def add_classicon(base, shipclass, color, fontsize):
-	base = base.rotate(-90)
-	fontpath = "./WoWsSymbol.ttf"
-	font = ImageFont.truetype(fontpath, fontsize)
-	draw = ImageDraw.Draw(base)
-	size = draw.textsize(shipclass, font=font)
-	pos = tuple([(a-b)//2 for a, b in zip(img_size, size)])
-	draw.text(pos, shipclass, font=font, fill=color)
-	base = base.rotate(90)
-	return base
+    base = base.rotate(-90)
+    fontpath = "./WoWsSymbol.ttf"
+    font = ImageFont.truetype(fontpath, fontsize)
+    draw = ImageDraw.Draw(base)
+    size = get_textsize(font, shipclass)
+    pos = tuple([(a-b)//2 for a, b in zip(img_size, size)])
+    draw.text(pos, shipclass, font=font, fill=color)
+    base = base.rotate(90)
+    return base
 
 def add_shipname(base, name, color, fontsize):
-	font = ImageFont.truetype(fontface, fontsize)
-	draw = ImageDraw.Draw(base)
-	while draw.textsize(name, font=font)[0]>img_size[0]:
-		name = name[:-1]
-	size = draw.textsize(name, font=font)
-	pos = tuple([(a-b)//2 for a, b in zip(img_size, size)])
-	pos = (pos[0],pos[1]+125)
-	draw.text(pos, name, font=font, fill=color)
-	return base
+    font = ImageFont.truetype(fontface, fontsize)
+    draw = ImageDraw.Draw(base)
+    while get_textsize(font, name)[0]>img_size[0]:
+        name = name[:-1]
+    size = get_textsize(font, name)
+    pos = tuple([(a-b)//2 for a, b in zip(img_size, size)])
+    pos = (pos[0],pos[1]+125)
+    draw.text(pos, name, font=font, fill=color)
+    return base
 
 def main():
-	argv = sys.argv
-	if (argv[1] == "CV") or argv[1] == "cv":
-		shipclass = CV
-	elif (argv[1] == "BB") or (argv[1] == "bb"):
-		shipclass = BB
-	elif (argv[1] == "CA") or (argv[1] == "CL") or (argv[1] == "ca") or (argv[1] == "cl"):
-		shipclass = CA
-	elif (argv[1] == "DD") or (argv[1] == "dd"):
-		shipclass = DD
-	else:
-		raise Exception("Invalid class name. (CV|BB|CA|CL|DD|cv|bb|ca|cl|dd)")
-	if (argv[2] == "enemy"):
-		color = enemy
-	elif (argv[2] == "ally"):
-		color = ally
-	name = argv[3]
-	base = Image.new("RGBA", img_size, color=base_color)
-	base = add_classicon(base, shipclass, color, icon_size)
-	base = add_shipname(base, name, color, 100)
-	filename = argv[4]
-	base.save(filename)
+    argv = sys.argv
+    if (argv[1] == "CV") or argv[1] == "cv":
+        shipclass = CV
+    elif (argv[1] == "BB") or (argv[1] == "bb"):
+        shipclass = BB
+    elif (argv[1] == "CA") or (argv[1] == "CL") or (argv[1] == "ca") or (argv[1] == "cl"):
+        shipclass = CA
+    elif (argv[1] == "DD") or (argv[1] == "dd"):
+        shipclass = DD
+    else:
+        raise Exception("Invalid class name. (CV|BB|CA|CL|DD|cv|bb|ca|cl|dd)")
+    if (argv[2] == "enemy"):
+        color = enemy
+    elif (argv[2] == "ally"):
+        color = ally
+    name = argv[3]
+    base = Image.new("RGBA", img_size, color=base_color)
+    base = add_classicon(base, shipclass, color, icon_size)
+    base = add_shipname(base, name, color, 100)
+    filename = argv[4]
+    base.save(filename)
 
 if __name__ == '__main__':
-	main()
+    main()
