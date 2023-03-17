5115174e1677add5506c7d9869558f869a55d1d7
void-hoge <coilgunrailgun@gmail.com>
2023-03-17 22:20:11 +0900

    remake

--- README.md
+++ README.md
@@ -1,27 +1,23 @@
 # SHIP ICON GENERATOR
 - タク作るときの船のアイコンを生成するやつ
 
-![VOIDHOGE.png](VOIDHOGE.png)
+![SLAVA.png](icons/battleship/ally/SLAVA.png)
 
-## INSTALLATION
-- Pythonの画像編集ライブラリのPillowが必要
+## Required
+- Pillow
   - `pip3 install Pillow`
-- 船の名前用のフォントとして、Roboto-Mediumを使っている。
-  - システムフォントにない場合はインストールするか、別のフォントをmain.pyの5行目で指定する必要がある。
+- Roboto-Medium
 
-## USAGE
-- `./main.py (CV|BB|CA|CL|DD|cv|bb|ca|cl|dd) (enemy|ally) <name> <filename>`
-- 名前に空白を含む場合は、"DES MOINES"のようにダブルクウォートで囲むと吉。
-- T10の艦名リスト((cv|bb|ca|dd)_list.txt)が付属しているので、それを使って`./generate`すると良い感じに生成してくれる。(生成済み)
-  - リストを編集して自由に船を増やせる。
-  - その際全て再生成されるので注意
-- allships/に0.10.10の全艦リストを追加した。
-  - これを得るためのjsonviwerのクエリをallships/query.txtに書いた。
+## Usage
+- `./shipicon.py --shiplist=<shiplist> --shipclass=(submarine|destroyer|cruiser|battleship)`
+- デフォルトでは`icons/<shipclass>/<ally|enemy>`にそれぞれの名前の船のアイコンを生成する。
+- T10と11の艦名リスト`(cv|bb|ca|dd|ss)_list.txt`が付属している。
+  - `./generate`で、それを使って自動生成してくれる。
 
 ## LICENSE
-- 艦の名前及びID、アイコン(文字部分を除く記号)は、WGの著作物である。よって、以下のガイドラインに基づいてこのリポジトリを公開する。
+- 以下のガイドラインに基づく
   - 日本語: https://worldofwarships.asia/ja/news/general-news/content-creator-guidelines/
   - English: https://worldofwarships.asia/en/news/general-news/content-creator-guidelines/
-- このプログラムなどを頒布、改変した上で頒布する場合、上のガイドラインに適合する必要がある。
-- 私(IGN:voidhoge, Github:void-hoge, Twitter:@voidhoge, Discord: void-hoge#5115)はこのリポジトリに含まれるデータ、プログラム、及びプログラムによって生成されたデータに対してなんの責任も負わない。
-- 一部にテスト艦の名前を含むが、これは全てdevelopment blogの内容による。テスト中に名前が変更されることがある。また、一時的なテストのためのテスト艦で、今後実装されないものも含まれる。(Brennus, Lapplandなど)
+
+## Author
+- void-hoge (Discord: void-hoge#5115, Twitter: @voidhoge)
---
+++ shipicon.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+
+from PIL import Image, ImageFont, ImageDraw
+import argparse
+import os
+
+FONTFACE = 'Roboto-Medium.ttf'
+FONTSIZE = 100
+IMAGESIZE = 400, 400
+ALLY_COLOR = 0, 255, 0
+ENEMY_COLOR = 255, 69, 69
+BACKGROUND_COLOR = 0, 0, 0, 0
+VERTPATH = 'vertices/'
+SCALE = 5.0
+
+class shipicon:
+    def __init__(self):
+        self.classicon = {}
+        for shipclass in ['submarine','destroyer',
+                          'cruiser', 'battleship',
+                          'aircarrier']:
+            with open(f'{VERTPATH}{shipclass}.vertices', 'r') as vfile:
+                verts = vfile.read().replace('\n', '')
+                center, polygon = eval(verts)
+                icon = {'center': center,
+                        'polygon': polygon}
+                self.classicon[shipclass] = icon
+        self.fontface = ImageFont.truetype(FONTFACE, FONTSIZE)
+
+    def render(self, size, shipclass, shipname, color, filename=None):
+        base = Image.new('RGBA', size, color=BACKGROUND_COLOR)
+        base = self.draw_classicon(size, base, shipclass, color)
+        base = self.draw_shipname(size, base, shipname, color)
+        if filename:
+            base.save(filename)
+        else:
+            base.save(f'{shipname}.png')
+
+    def draw_classicon(self, size, base, shipclass, color):
+        draw = ImageDraw.Draw(base, 'RGBA')
+        iwidth, iheight = size
+        center = self.classicon[shipclass]['center']
+        polygons = self.classicon[shipclass]['polygon']
+        cy, cx = center
+        for polygon in polygons:
+            poly = [((x-cx)*SCALE+iwidth//2, (y-cy)*SCALE+iheight//2) for x,y in polygon]
+            draw.polygon(poly, fill=color)
+        return base
+
+    def getbbox(self, string): # width, height
+        left, top, right, bottom = self.fontface.getbbox(string)
+        return right-left, bottom-top
+
+    def draw_shipname(self, size, base, shipname, color):
+        draw = ImageDraw.Draw(base)
+        left, top, right, bottom = self.fontface.getbbox(shipname)
+        iwidth, iheight = IMAGESIZE
+        while self.getbbox(shipname)[0] > iwidth:
+            shipname = shipname[:-1]
+        while shipname and shipname[-1] == ' ':
+            shipname = shipname[:-1]
+        left, top, right, bottom = self.fontface.getbbox(shipname)
+        width, height = right-left, bottom-top
+        pos = (iwidth-width)//2, iheight-bottom
+        draw.text(pos, shipname, font=self.fontface, fill=color)
+        return base
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--shiplist', type=str, required=True)
+    parser.add_argument('--shipclass', type=str, required=True)
+    parser.add_argument('--size', type=int, required=False)
+    arg = parser.parse_args()
+    filename = arg.shiplist
+    shipclass = arg.shipclass
+    if arg.size:
+        size = arg.size, arg.size
+    else:
+        size = IMAGESIZE
+    icongen = shipicon()
+    with open(filename, 'r') as shipsfile:
+        shiplist = shipsfile.read().split('\n')
+        for side, color in [('enemy',ENEMY_COLOR), ('ally', ALLY_COLOR)]:
+            directory = f'icons/{shipclass}/{side}'
+            try:
+                os.makedirs(directory)
+            except FileExistsError:
+                pass
+            icongen.render(
+                size=size,
+                shipclass=shipclass,
+                shipname='',
+                color=color,
+                filename=f'{directory}/UNNAMED.png')
+            print(f'{directory}/UNNAMED.png')
+            for ship in shiplist:
+                if ship:
+                    icongen.render(
+                        size=size,
+                        shipclass=shipclass,
+                        shipname=ship,
+                        color=color,
+                        filename=f'{directory}/{ship}.png')
+                    print(f'{directory}/{ship}.png')
+
+if __name__ == '__main__':
+    main()
